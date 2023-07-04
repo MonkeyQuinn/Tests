8430083e1cc31d9f3fc3f8863c0d9677c5ee5dac
Yaha <yahyarov_iliyar@mail.ru>
2023-07-04 10:40:40 +0600

    Завершил тест String1

--- src/String1.java
+++ src/String1.java
@@ -113,4 +113,187 @@ public class String1 {
 
         return str;
     }
+
+    public static boolean hasBad(String str) {
+        return str.indexOf("bad") == 1 || str.indexOf("bad") == 0;
+    }
+
+    public static String atFirst(String str) {
+        if (str.length() >= 2) {
+            str = str.substring(0, 2);
+
+        } else {
+            int n = str.length();
+            for (int i = 0; i < 2 - n; i++) {
+                str += "@";
+            }
+        }
+
+        return str;
+    }
+
+    public static String lastChars(String a, String b) {
+        if (a.length() > 0) {
+            a = a.substring(0, 1);
+
+        } else {
+            a = "@";
+        }
+
+        if (b.length() > 0) {
+            b = b.substring(b.length() - 1);
+
+        } else {
+            b = "@";
+        }
+
+        return a + b;
+    }
+
+    public static String conCat(String a, String b) {
+        if (a.length() != 0 && b.length() != 0) {
+            if (a.charAt(a.length() - 1) == b.charAt(0)) {
+                a = a.substring(0, a.length() - 1);
+            }
+        }
+
+        return a + b;
+    }
+
+    public static String lastTwo(String str) {
+        if (str.length() >= 2) {
+            str = str.substring(0, str.length() - 2) + str.charAt(str.length() - 1) + str.charAt(str.length() - 2);
+        }
+
+        return str;
+    }
+
+    public static String seeColor(String str) {
+        if (str.indexOf("red") == 0) {
+            return "red";
+
+        } else if (str.indexOf("blue") == 0) {
+            return "blue";
+
+        } else {
+            return "";
+        }
+    }
+
+    public static boolean frontAgain(String str) {
+        if (str.length() >= 2) {
+            String f = str.substring(0, 2);
+            String l = str.substring(str.length() - 2);
+
+            return f.equals(l);
+        }
+
+        return false;
+    }
+
+    public static String minCat(String a, String b) {
+        if (a.length() != b.length()) {
+            if (a.length() > b.length()) {
+                a = a.substring(a.length() - b.length());
+
+            } else {
+                b = b.substring(b.length() - a.length());
+            }
+        }
+
+        return a + b;
+    }
+
+    public static String extraFront(String str) {
+        if (str.length() > 2) {
+            str = str.substring(0, 2);
+        }
+
+        return str + str + str;
+    }
+
+    public static String startWord(String str, String word) {
+        if (str.length() > 0 && str.length() >= word.length()) {
+            word = word.substring(1);
+
+            if (word.equals(str.substring(1, word.length() + 1))) {
+                return str.substring(0, word.length() + 1);
+            }
+        }
+
+        return "";
+    }
+
+    public static String withoutX(String str) {
+        if (str.contains("x") && str.charAt(0) == 'x') {
+            str = str.substring(1);
+        }
+
+        if (str.contains("x") && str.charAt(str.length() - 1) == 'x') {
+            str = str.substring(0, str.length() - 1);
+        }
+
+        return str;
+    }
+
+    public static String withoutX2(String str) {
+        if (str.length() >= 2) {
+            for (int i = 1; i >= 0; i--) {
+                if (str.charAt(i) == 'x') {
+                    str = str.replaceFirst("x", "");
+                }
+            }
+
+        } else {
+            str = str.replaceAll("x", "");
+        }
+
+        return str;
+    }
+
+    public static String makeAbba(String a, String b) {
+        return a + b + b + a;
+    }
+
+    public static String without2(String str) {
+        if (str.length() >= 2) {
+            if (str.substring(0, 2).equals(str.substring(str.length() - 2))) {
+                return str.substring(2);
+            }
+        }
+
+        return str;
+    }
+
+    public static String makeTags(String tag, String word) {
+        return "<" + tag + ">" + word + "</" + tag + ">";
+    }
+
+    public static String deFront(String str) {
+        if (str.length() > 2) {
+            char firstLetter = str.charAt(0);
+            char secondLetter = str.charAt(1);
+
+            str = str.substring(2);
+
+            if (secondLetter == 'b') {
+                str = "b" + str;
+            }
+
+            if (firstLetter == 'a') {
+                str = "a" + str;
+            }
+
+        } else {
+            if (str.indexOf("b") != 1) {
+                str = str.substring(0, 1);
+            }
+
+            if (str.indexOf("a") != 0) {
+                str = str.substring(1);
+            }
+        }
+
+        return str;
+    }
 }
